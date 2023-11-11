e673aab0a0f5cf56afd80af487ed90826b762851
Benjamin Vergara <aquantumguy@gmail.com>
2023-11-11 17:56:46 -0600

    Login and Signup

---
+++ webScraper/frontEnd/js/login.js
@@ -0,0 +1,23 @@
+import { loginUser } from '../controllers/user.js';
+
+$(document).ready(function () {
+    const form = $('#login-form');
+
+    form.on('submit', async function (e) {
+        e.preventDefault();
+
+        const email = $('#email1').val();
+        const password = $('#password1').val();
+
+        console.log(email,password);
+
+        try {
+            const responseData = await loginUser(email, password);
+            window.location.href = '../views/index.html';
+        } catch (error) {
+            alert('Something went wrong');
+            console.error(error);
+        }
+    });
+});
+
