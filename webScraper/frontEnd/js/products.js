e5e22194e311819f88d5114993459ba05db605b8
dedios.lopez@iteso.mx <dedios.lopez@iteso.mx>
2023-11-20 16:50:22 -0600

    fill in products page

--- webScraper/frontEnd/js/products.js
+++ webScraper/frontEnd/js/products.js
@@ -6,28 +6,60 @@ $(document).ready(() =>{
     getProducts().then((value) => {
         console.log(value);
         // Expected output: "Success!"
+        const baseDiv = document.getElementById("baseGalery");
+        $.each(value, function(indice, entry){
+            console.log(indice)
+            console.log(entry)
+            const element = document.createElement("div");
+            element.classList.add("producto");
+            element.setAttribute("id", entry.id);
+            baseDiv.appendChild(element);
+
+            const imagen = document.createElement("img");
+            // element.classList.add("producto");
+            imagen.setAttribute("src", entry.linkImg);
+            imagen.setAttribute("alt", "Producto");
+            element.appendChild(imagen);
+
+            const brSimple01 = document.createElement("br");
+            // element.classList.add("producto");
+            element.appendChild(brSimple01);
+
+            const span01 = document.createElement("span");
+            span01.classList.add("precioC");
+            span01.innerHTML = "$ " + entry.precio;
+            element.appendChild(span01);
+
+            const brSimple02 = document.createElement("br");
+            // element.classList.add("producto");
+            element.appendChild(brSimple02);
+
+            const span02 = document.createElement("span");
+            span02.classList.add("descuento");
+            span02.innerHTML = entry.descuento + " %";
+            element.appendChild(span02);
+
+            const brSimple03 = document.createElement("br");
+            // element.classList.add("producto");
+            element.appendChild(brSimple03);
+
+            
+            const nombre = document.createElement("span");
+            nombre.classList.add("nombre");
+            nombre.innerHTML = entry.nombreProducto;
+            element.appendChild(nombre);
+
+            const brSimple04 = document.createElement("br");
+            // element.classList.add("producto");
+            element.appendChild(brSimple04);
+
+            const verProducto = document.createElement("a");
+            verProducto.classList.add("ver");
+            verProducto.setAttribute("href", "verProducto.html");
+            verProducto.innerHTML = "Ver";
+            element.appendChild(verProducto);
+
+        });
       });
 
-/*     $('#importJson').click(function() {
-        console.log("Gra is here");
-         $.ajax({
-            type: 'GET',
-            url: 'https://jsonplaceholder.typicode.com/albums',
-            dataType: 'json'
-        
-        }).done((data) => {
-            $.each(data, function(indice, album){
-                let fila = $('<tr>');
-                fila.append($(`<td>${album.userId} </td>`));
-                fila.append($(`<td>${album.id} </td>`));
-                fila.append($(`<td>${album.title} </td>`));
-
-                $('#albums tbody').append(fila);
-                
-                
-            });
-
-            $('#albums').show();
-        }) 
-    }) */
 })
