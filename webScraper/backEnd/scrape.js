d514d1f054d8b1b0bf617ce5bafb1ad48d349afb
dedios.lopez@iteso.mx <dedios.lopez@iteso.mx>
2023-11-20 13:34:33 -0600

    se corrigió el update

--- webScraper/backEnd/scrape.js
+++ webScraper/backEnd/scrape.js
@@ -1,75 +1,52 @@
-const request = require('request');
-const cheerio = require('cheerio');
+
+const request = require("request");
+const cheerio = require("cheerio");
 const fs = require("fs");
 
-const file = fs.readFileSync('products.json')
-var myObject = JSON.parse(file); 
-const { writeFileSync } = require('fs');
-
-const link = 'https://www.mercadolibre.com.mx/xiaomi-pocophone-poco-m5s-dual-sim-256-gb-gris-8-gb-ram/p/MLM23428712?pdp_filters=deal%3AMLM779363-1#polycard_client=homes-korribanSearchPromotions&searchVariation=MLM23428712&position=1&search_layout=grid&type=product&tracking_id=1c310ba7-c260-465a-a5ac-d79e49688214';
-var previousPrice = 0;
-var productDescount = 0;
-
-const loadProducts = request(link, (error, 
-response, html) => {
-    if(!error && response.statusCode == 200) {
+var myObject = []; // JSON.parse(file);
+const { writeFileSync } = require("fs");
 
 
+module.exports = {
+  scrapeProduct: function (link,previousPrice,productDescount ) {
+    request(link, (error, response, html) => {
+
+      if (!error && response.statusCode == 200) {
         const $ = cheerio.load(html);
 
-        const productName = $('.ui-pdp-title').text();
+        const productName = $(".ui-pdp-title").text();
         console.log(productName);
 
-        const productPrice = $('[itemprop="price"]').attr('content');
+        const productPrice = $('[itemprop="price"]').attr("content");
         console.log(productPrice);
 
-        const productImg = $('img.ui-pdp-image.ui-pdp-gallery__figure__image').attr('src');
+        const productImg = $(
+          "img.ui-pdp-image.ui-pdp-gallery__figure__image"
+        ).attr("src");
         console.log(productImg);
 
-        if (previousPrice === 0){
-          previousPrice = productPrice;
-        }else if (previousPrice >= productPrice){
-          productDescount = 0;
-        }else{
-          productDescount = productPrice - previousPrice
-        }
-        
-        
-        
-
         // Add new Product to Json
-        const infoProducts = {precio : productPrice, 
-                              descuento : productDescount,
-                              precioAnterior : previousPrice,
-                              nombreProducto : productName, 
-                              linkProducto : link,
-                              linkImg : productImg
+        const infoProducts = {
+          precio : productPrice, 
+          descuento : productDescount,
+          precioAnterior : previousPrice,
+          nombreProducto : productName, 
+          link : link,
+          linkImg : productImg
 
-                            }
+        }
 
-        /* console.log(JSON.stringify(infoProducts));
-        
-        console.log("JSON ORIGINAL");
-        console.log(JSON.stringify(myObject));
-         */
         console.log("JSON NEW");
         myObject.push(infoProducts);
         console.log(JSON.stringify(myObject));
 
         try {
-            writeFileSync('./products.json', JSON.stringify(myObject), 'utf8');
-            console.log('Data successfully saved to disk'); 
-          } catch (error) {
-            console.log('An error has occurred ', error);
-          }
-        
-    }
-
-
-}) 
-
-
-module.exports = loadProducts;
-
-
-
+          writeFileSync("./products.json", JSON.stringify(myObject), "utf8");
+          console.log("Data successfully saved to disk");
+        } catch (error) {
+          console.log("An error has occurred ", error);
+        }
+      }
+    });
+  },
+};
--- webScraper/backEnd/updateProducts.js
+++ webScraper/backEnd/updateProducts.js
@@ -1,46 +1,35 @@
-const request = require('request');
-const cheerio = require('cheerio');
+const request = require("request");
+const cheerio = require("cheerio");
 const fs = require("fs");
 
-//const {loadProducts} = require('./scrape.js');
 
-const file = fs.readFileSync('products.json')
-var products = JSON.parse(file); 
+const scraper = require("./scrape.js");
 
-const galeria = $("#galeria");
- 
+fd = fs.openSync("products.json")
+const file = fs.readFileSync(fd);
+var products = JSON.parse(file);
+fs.closeSync(fd);
 
+fs.truncateSync("products.json");
 
+const link = "https://www.mercadolibre.com.mx/xiaomi-pocophone-poco-m5s-dual-sim-256-gb-gris-8-gb-ram/p/MLM23428712?pdp_filters=deal%3AMLM779363-1#polycard_client=homes-korribanSearchPromotions&searchVariation=MLM23428712&position=1&search_layout=grid&type=product&tracking_id=1c310ba7-c260-465a-a5ac-d79e49688214";
 
 function iterateProducts(products) {
-    products.forEach(product => {
 
-      //loadProducts.request(product.link);
-      // Access individual properties of each product
-      console.log("Product ID:", product.id || "N/A");
-      console.log("Product Link:", product.link || "N/A");
-      console.log("Product Name:", product.nombreProducto || "N/A");
-      console.log("Product Price:", product.precio || "N/A");
-      console.log("Product Image Link:", product.linkImg || "N/A");
-      console.log("------------------------");
+  products.forEach((product) => {
+    scraper.scrapeProduct(product.link, product.previousPrice, product.productDescount);
 
-      var obj = '<div class="producto">' + 
-                '<img src="' + product.linkImg + '" alt="Producto"> </img>' +
-                '<br>' +
-                '<span class="precioC">' + product.precio + '</span> ' + 
-                '<br>' +
-                '<span class="descuento">' + product.precio + '</span>' +
-                '<br>' +
-                '<span class="nombre">' + product.nombreProducto + '</span>' + 
-                '<br>' +
-                '<a href="verProducto.html" id="ver"> Ver </a>' +
-                '</div>';
+    // Access individual properties of each product
+    console.log("Product ID:", product.id || "N/A");
+    console.log("Product Prev Price:", product.previousPrice || "N/A");
+    console.log("Product Descount:", product.productDescount || "N/A");
 
-      console.log("OBJ: "+obj);
+    console.log("Product Link:", product.link || "N/A");
+    console.log("Product Name:", product.nombreProducto || "N/A");
+    console.log("Product Price:", product.precio || "N/A");
+    console.log("Product Image Link:", product.linkImg || "N/A");
+    console.log("------------------------");
+  });
+}
 
-      galeria.innerHTML = obj;
-    });
-  }
-  
-
-  iterateProducts(products);
+iterateProducts(products);
