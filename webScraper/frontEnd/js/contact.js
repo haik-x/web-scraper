6f1c9c4ce13d8cc669fa953713a5f5551d3debda
Benjamin Vergara <aquantumguy@gmail.com>
2023-11-27 17:38:08 -0600

    Contact form functioning properly

---
+++ webScraper/frontEnd/js/contact.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+    const form = $("#contact-form");
+    const username = $("#username");
+    const email = $("#email");
+    const message = $("#message");
+
+    form.on('submit', async function (e) {
+        e.preventDefault();
+        checkInputs();
+
+        if (!username.hasClass("error") && !email.hasClass("error") && !message.hasClass("error")) {
+            const bodyMesssage = `Name: ${username.val()} <br> Email: ${email.val()} <br> Message: ${message.val()}`; // Updated variable name
+
+            Email.send({
+                SecureToken: "a2c1beca-4507-4bbe-86b1-b37f9415446a",
+                To: 'pweb1384@gmail.com',
+                From: "pweb1384@gmail.com",
+                Subject: "Contact Request",
+                Body: bodyMesssage
+            }).then(
+                message => {
+                    if (message === "OK") {
+                        Swal.fire({
+                            title: "OK!",
+                            text: "¡Mensaje enviado con éxito!",
+                            icon: "success",
+                            confirmButtonColor: '#00008b'
+                        });
+                    }
+                }
+            );
+
+            form[0].reset(); // Use form[0] to access the native DOM element
+            return false;
+        }
+    });
+});
+
+function checkInputs() {
+    const items = $(".form-control");
+
+    items.each(function () {
+        $(this).on("input", function () {
+            validateInput($(this));
+        });
+
+        // Initial validation
+        validateInput($(this));
+    });
+
+    function validateInput(input) {
+        if (input.val().trim() === "") {
+            input.addClass("error");
+            input.parent().addClass("error");
+        } else {
+            input.removeClass("error");
+            input.parent().removeClass("error");
+        }
+    }
+}
