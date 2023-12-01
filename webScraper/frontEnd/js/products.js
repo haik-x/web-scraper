79c67d0c83a95a46c3154b047833f4a60f78ff98
LoreRuelas <lore.ruelasg@gmail.com>
2023-12-01 16:26:14 -0600

    mejora en allProducts.css

--- webScraper/frontEnd/js/products.js
+++ webScraper/frontEnd/js/products.js
@@ -30,72 +30,72 @@ $(document).ready(() =>{
             element.setAttribute("id", entry.id);
             baseDiv.appendChild(element);
 
-            const imagen = document.createElement("img");
-            // element.classList.add("producto");
-            imagen.setAttribute("src", entry.linkImg);
-            imagen.setAttribute("alt", "Producto");
-            element.appendChild(imagen);
+            const imgDiv = document.createElement("div");
+            imgDiv.setAttribute("class", "imgDiv");
+            element.appendChild(imgDiv);
 
-            const brSimple01 = document.createElement("br");
-            // element.classList.add("producto");
-            element.appendChild(brSimple01);
+                const imagen = document.createElement("img");
+                imagen.setAttribute("src", entry.linkImg);
+                imagen.setAttribute("alt", "Producto");
+                imgDiv.appendChild(imagen);
 
-            const span01 = document.createElement("span");
-            span01.classList.add("precioC");
-            span01.innerHTML = "$ " + entry.precio;
-            element.appendChild(span01);
+            const infoDiv = document.createElement("div");
+            infoDiv.setAttribute("class", "infoDiv");
+            element.appendChild(infoDiv);
+                const brSimple06 = document.createElement("br");
+                infoDiv.appendChild(brSimple06);
 
-            const brSimple02 = document.createElement("br");
-            // element.classList.add("producto");
-            element.appendChild(brSimple02);
+                const nombre = document.createElement("span");
+                nombre.classList.add("nombre");
+                nombre.innerHTML = entry.nombreProducto;
+                infoDiv.appendChild(nombre);
 
-            const span02 = document.createElement("span");
-            span02.classList.add("descuento");
-            span02.innerHTML = entry.descuento + " %";
-            element.appendChild(span02);
+                const brSimple04 = document.createElement("br");
+                infoDiv.appendChild(brSimple04);
 
-            const brSimple03 = document.createElement("br");
-            // element.classList.add("producto");
-            element.appendChild(brSimple03);
+                const brSimple01 = document.createElement("br");
+                infoDiv.appendChild(brSimple01);
 
-            
-            const nombre = document.createElement("span");
-            nombre.classList.add("nombre");
-            nombre.innerHTML = entry.nombreProducto;
-            element.appendChild(nombre);
+                const span01 = document.createElement("span");
+                span01.classList.add("precioC");
+                span01.innerHTML = "Precio $ " + entry.precio;
+                infoDiv.appendChild(span01);
 
-            const brSimple04 = document.createElement("br");
-            // element.classList.add("producto");
-            element.appendChild(brSimple04);
+                const brSimple02 = document.createElement("br");
+                infoDiv.appendChild(brSimple02);
 
-            const verProducto = document.createElement("a");
-            verProducto.classList.add("ver");
-            verProducto.setAttribute("href", "verProducto.html");
-            verProducto.innerHTML = "Ver";
-            element.appendChild(verProducto);// button
+                const span02 = document.createElement("span");
+                span02.classList.add("descuento");
+                span02.innerHTML = "Descuento " + entry.descuento + " %";
+                infoDiv.appendChild(span02);
 
-            const brSimple05 = document.createElement("br");
-            // element.classList.add("producto");
-            element.appendChild(brSimple05);
+                const brSimple03 = document.createElement("br");
+                infoDiv.appendChild(brSimple03);
 
-            const updateOption = document.createElement("button");
-            updateOption.setAttribute("id", "botonUpdate");
-            updateOption.innerHTML = "Update";
-            element.appendChild(updateOption);
-            updateOption.addEventListener('click', (event) => {
-                updateProduct(event.currentTarget.parentElement.id);
-                location.reload(true);
-              });
+                const brSimple05 = document.createElement("br");
+                infoDiv.appendChild(brSimple05);
 
-              const deleteOption = document.createElement("button");
-              // element.classList.add("producto");
-              deleteOption.setAttribute("id", "botonDelete");
-              deleteOption.innerHTML = "Delete";
-              element.appendChild(deleteOption);
-              deleteOption.addEventListener('click', (event) => {
-                  deleteProduct(event.currentTarget.parentElement.id);
-                  location.reload(true);
+            const actionsDiv = document.createElement("div");
+            actionsDiv.setAttribute("class", "actionsDiv");
+            element.appendChild(actionsDiv);
+
+                const updateOption = document.createElement("button");
+                updateOption.setAttribute("id", "botonUpdate");
+                updateOption.innerHTML = "Update";
+                actionsDiv.appendChild(updateOption);
+                updateOption.addEventListener('click', (event) => {
+                    updateProduct(event.currentTarget.parentElement.id);
+                    location.reload(true);
                 });
+
+                const deleteOption = document.createElement("button");
+                deleteOption.setAttribute("id", "botonDelete");
+                deleteOption.innerHTML = "Delete";
+                actionsDiv.appendChild(deleteOption);
+                deleteOption.addEventListener('click', (event) => {
+                    deleteProduct(event.currentTarget.parentElement.id);
+                    location.reload(true);
+                    });
 
         });
       });
